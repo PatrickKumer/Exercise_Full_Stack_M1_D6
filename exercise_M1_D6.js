7b660e979c841293d175781800293aa5a3f81dd7
PatrickKumer <patrick.kumer@gmail.com>
2022-01-31 09:46:23 +0100

    Solved exercise B

--- exercise_M1_D6.js
+++ exercise_M1_D6.js
@@ -29,10 +29,20 @@ console.log(test)
     Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
 */
 printTitle("Exercise B")
+
+let sum = 10 + 20
+
+console.log(sum)
+
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 printTitle("Exercise C")
+
+let random = Math.floor(Math.random() * 10)
+
+console.log(random)
+
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
