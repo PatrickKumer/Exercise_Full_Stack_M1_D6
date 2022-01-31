7be3619de9b829812a1ed45d9f73f62762e7958b
PatrickKumer <patrick.kumer@gmail.com>
2022-01-31 09:58:46 +0100

    Solved exercise F

--- exercise_M1_D6.js
+++ exercise_M1_D6.js
@@ -65,11 +65,15 @@ delete me.age
 
 console.log(me)
 
-
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
 printTitle("Exercise F")
+
+me.skills = ["HTML", "CSS", "JavaScript"]
+
+console.log(me)
+
 /* EXERCISE G
    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
 */
